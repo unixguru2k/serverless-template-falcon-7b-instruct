fa35ea0125193d77c03863a566ee8dc6090508d8
Harshal Dhir <unixguru2k@ip-192-168-1-108.ec2.internal>
2023-06-12 20:17:51 -0700

    trying with falcon - testing

--- download.py
+++ download.py
@@ -8,10 +8,8 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    # tokenizer = AutoTokenizer.from_pretrained("tiiuae/falcon-7b-instruct", trust_remote_code=True)
-    # model = AutoModelForCausalLM.from_pretrained("tiiuae/falcon-7b-instruct", trust_remote_code=True)
-    print(f"skipping...")
-
+    AutoTokenizer.from_pretrained("tiiuae/falcon-7b-instruct", trust_remote_code=True)
+    AutoModelForCausalLM.from_pretrained("tiiuae/falcon-7b-instruct", trust_remote_code=True)
 
 if __name__ == "__main__":
     download_model()
